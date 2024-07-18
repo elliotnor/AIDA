cfe1809bbe4f32cea28dfecafadc3984142ce50b
Elliotnor <121126200+elliotnor@users.noreply.github.com>
2024-07-18 12:47:11 +0200

    Delete android 2 directory

--- android 2/app/src/main/java/com/example/aida/enums/ConnectionStages.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aida.enums
-
-// Enums used to describe connection connection stages of AIDA API
-enum class ConnectionStages {
-    CONNECTING,
-    CONNECTION_FAILED,
-    CONNECTION_SUCCEEDED,
-    CONNECTION_CLOSED
-}
